2b6cc4189be049eb89f8b72b766860568bc900f6
Jedidiah-16 <you@example.com>
2024-07-19 02:25:19 -0700

    Added settings and storage files for queries

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "AIzaSyAwwYyJht9kA086LZSuArBUo6muJyjmUgw"
+SEARCH_ID = "d5d88d720d58b488b"
+COUNTRY = "ng"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}" + COUNTRY
+RESULT_COUNT = 10
