96e497d8a76fd1d7c7c597bb44351966c7c0bd6b
Jedidiah-16 <you@example.com>
2024-07-19 13:22:47 -0700

    Update 'search.py'

--- search.py
+++ search.py
@@ -3,3 +3,23 @@ import requests
 from requests.exceptions import RequestException
 import pandas as pd
 from storage import DBStorage
+from urllib.parse import quote_plus
+
+def search_api(query, pages=int(RESULT_COUNT/2)):
+    results = []
+    for i in range(0, pages):
+        start = i * 2 + i
+        url = SEARCH_URL.format(
+            key=SEARCH_KEY,
+            cx=SEARCH_ID,
+            query=quote_plus(query),
+            start=start
+        )
+        response = requests.get(url)
+        data = response.json()
+
+        results += data["items"]
+    res_df = pd.DataFrame.from_dict(results)
+    res_df["rank"] = list(range(1, res_df.shape[0] + 1))
+    res_df = res_df[["link", "rank", "snippet", "title"]]
+    return res_df
