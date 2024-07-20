816079c4a7fbefd8b7e4a66a1793d4155a6449bc
Jedidiah-16 <you@example.com>
2024-07-19 22:07:09 -0700

    Created app.py removed search.py, storage.py and settins.py. Added HTML files for search page and to display search results

--- settings.py
+++
@@ -1,6 +0,0 @@
-SEARCH_KEY = "AIzaSyAwwYyJht9kA086LZSuArBUo6muJyjmUgw"
-SEARCH_ID = "c6c345c2e5da2449f"
-COUNTRY = "us"
-
-SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}" + COUNTRY
-RESULT_COUNT = 100
